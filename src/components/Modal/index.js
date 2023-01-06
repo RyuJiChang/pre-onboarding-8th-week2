c4d1a0c9bca6f38c73a4913027003b2b13897023
RyuJiChang <rgc0582@gmail.com>
2023-01-06 10:51:09 +0900

    feat:Update 기능 구현

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -12,17 +12,72 @@ function Modal({ isOpen, onClose, listData, data, setData }) {
     }
   };
   useEffect(() => {
-    console.log(listData);
     setModifyData({ ...listData });
   }, [listData]);
 
   const modifyOn = () => {
-    console.log(modifyData);
     setIsModify(true);
   };
 
-  const saveList = () => {
+  const saveList = e => {
+    e.preventDefault();
+
+    if (listData.listState === modifyData.listState) {
+      let newArr = [...data[listData.listState + 'Arr']].map(el => {
+        if (el.id === modifyData.id) {
+          return { ...modifyData };
+        } else {
+          return el;
+        }
+      });
+      if (modifyData.listState === 'todo') {
+        setData({
+          todoArr: [...newArr],
+          inProgressArr: [...data.inProgressArr],
+          doneArr: [...data.doneArr],
+          idNow: data.idNow,
+        });
+      } else if (modifyData.listState === 'inProgress') {
+        setData({
+          todoArr: [...data.todoArr],
+          inProgressArr: [...newArr],
+          doneArr: [...data.doneArr],
+          idNow: data.idNow,
+        });
+      } else if (modifyData.listState === 'done') {
+        setData({
+          todoArr: [...data.todoArr],
+          inProgressArr: [...data.inProgressArr],
+          doneArr: [...newArr],
+          idNow: data.idNow,
+        });
+      }
+    } else {
+      if (modifyData.listState === 'todo') {
+        setData({
+          todoArr: [...data.todoArr, modifyData],
+          inProgressArr: [...data.inProgressArr].filter(el => el.id !== listData.id),
+          doneArr: [...data.doneArr].filter(el => el.id !== listData.id),
+          idNow: data.idNow,
+        });
+      } else if (modifyData.listState === 'inProgress') {
+        setData({
+          todoArr: [...data.todoArr].filter(el => el.id !== listData.id),
+          inProgressArr: [...data.inProgressArr, modifyData],
+          doneArr: [...data.doneArr].filter(el => el.id !== listData.id),
+          idNow: data.idNow,
+        });
+      } else if (modifyData.listState === 'done') {
+        setData({
+          todoArr: [...data.todoArr].filter(el => el.id !== listData.id),
+          inProgressArr: [...data.inProgressArr].filter(el => el.id !== listData.id),
+          doneArr: [...data.doneArr, modifyData],
+          idNow: data.idNow,
+        });
+      }
+    }
     setIsModify(false);
+    onClose();
   };
 
   const cancleModify = () => {
@@ -56,6 +111,36 @@ function Modal({ isOpen, onClose, listData, data, setData }) {
     onClose();
   };
 
+  const modifyListData = e => {
+    console.log(e.target.value);
+    if ('todo inProgress done'.includes(e.target.id))
+      setModifyData({
+        ...modifyData,
+        listState: e.target.id,
+      });
+    else if (e.target.id === 'title') {
+      setModifyData({
+        ...modifyData,
+        title: e.target.value,
+      });
+    } else if (e.target.id === 'manager') {
+      setModifyData({
+        ...modifyData,
+        manager: e.target.value,
+      });
+    } else if (e.target.id === 'expiryDate') {
+      setModifyData({
+        ...modifyData,
+        expiryDate: e.target.value,
+      });
+    } else if (e.target.id === 'contents') {
+      setModifyData({
+        ...modifyData,
+        contents: e.target.value,
+      });
+    }
+  };
+
   return isOpen ? (
     <ModalContainer onClick={event => event.stopPropagation()}>
       <ModalBackdrop ref={outsideRef} onClick={closeModal}>
@@ -64,25 +149,62 @@ function Modal({ isOpen, onClose, listData, data, setData }) {
             <div onSubmit={saveList}>
               <form>
                 <div>
-                  <span>제목 : </span> <input type="text" />
+                  <span>제목 : </span>{' '}
+                  <input
+                    id="title"
+                    type="text"
+                    value={modifyData.title}
+                    onChange={modifyListData}
+                  />
                 </div>
                 <div>
                   <span>상태 : </span>
-                  <input id="Todo" name="state" type="radio" />
+                  <input
+                    checked={modifyData.listState === 'todo'}
+                    id="todo"
+                    name="state"
+                    type="radio"
+                    onChange={modifyListData}
+                  />
                   Todo
-                  <input id="InProgress" name="state" type="radio" />
+                  <input
+                    checked={modifyData.listState === 'inProgress'}
+                    id="inProgress"
+                    name="state"
+                    type="radio"
+                    onChange={modifyListData}
+                  />
                   In Progress
-                  <input id="Done" name="state" type="radio" />
+                  <input
+                    checked={modifyData.listState === 'done'}
+                    id="done"
+                    name="state"
+                    type="radio"
+                    onChange={modifyListData}
+                  />
                   Done
                 </div>
                 <div>
-                  <span>관리자 : </span> <input type="text" />
+                  <span>관리자 : </span>{' '}
+                  <input
+                    id="manager"
+                    type="text"
+                    value={modifyData.manager}
+                    onChange={modifyListData}
+                  />
                 </div>
                 <div>
-                  <span>마감일 : </span> <input type="datetime-local" />
+                  <span>마감일 : </span>{' '}
+                  <input
+                    id="expiryDate"
+                    type="datetime-local"
+                    value={modifyData.expiryDate}
+                    onChange={modifyListData}
+                  />
                 </div>
                 <div>
-                  <span>내용 : </span> <textarea />
+                  <span>내용 : </span>{' '}
+                  <textarea id="contents" value={modifyData.contents} onChange={modifyListData} />
                 </div>
                 <br />
                 <input type="submit" value={'저장'} />
