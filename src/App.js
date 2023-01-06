a6576d51f93abce8ee5694d2f0bfa77f682e031f
RyuJiChang <rgc0582@gmail.com>
2023-01-06 20:16:20 +0900

    feat:drag&drop 기능 구현

--- src/App.js
+++ src/App.js
@@ -66,6 +66,88 @@ function App() {
   const onClose = () => {
     setModalOpen(false);
   };
+
+  // const dragStart = (e, el) => {
+  //   console.log(index, el.listState);
+  //   // e.dataTransfer.setData('todoId', el.id);
+  // };
+  // const dragEnter = (e, el) => {
+  //   // let element = document.getElementById(el.id);
+  //   // console.log(element.getBoundingClientRect());
+  // };
+
+  const dragEnd = (e, el) => {
+    // console.log(el.listState, el.id); //start state
+
+    let index = 0;
+    let finalState;
+    let element = document.elementFromPoint(e.clientX, e.clientY);
+    //컨테이너로 지정될 경우 위치 파악
+    if (['todo', 'inProgress', 'done'].includes(element.id)) {
+      finalState = element.id;
+      for (let i = 0; i < element.childNodes.length; i++) {
+        let elementRect = element.childNodes[i].getBoundingClientRect();
+        if (e.clientY >= (elementRect.top + elementRect.bottom) / 2) {
+          if (String(el.id) !== element.childNodes[i].id) {
+            index++;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+    //리스트로 지정된 경우 리스트 index 파악
+    else {
+      finalState = element.parentElement.id;
+      for (let i = 0; i < element.parentElement.childNodes.length; i++) {
+        let elementRect = element.parentElement.childNodes[i].getBoundingClientRect();
+        if (e.clientY >= (elementRect.top + elementRect.bottom) / 2) {
+          if (String(el.id) !== element.childNodes[i].id) {
+            index++;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+    console.log(el.id, el.listState);
+    moveList(el.id, el.listState, index, finalState);
+  };
+
+  const moveList = (startId, startState, index, finalState) => {
+    // console.log('now moving start', startState, startId);
+    let newData;
+    let moveList = {
+      ...data[startState + 'Arr'].filter(el => el.id === startId)[0],
+      listState: finalState,
+    };
+    if (startState === 'todo') {
+      newData = {
+        todoArr: [...data.todoArr].filter(el => el.id !== startId),
+        inProgressArr: [...data.inProgressArr],
+        doneArr: [...data.doneArr],
+        idNow: data.idNow,
+      };
+    } else if (startState === 'inProgress') {
+      newData = {
+        todoArr: [...data.todoArr],
+        inProgressArr: [...data.inProgressArr].filter(el => el.id !== startId),
+        doneArr: [...data.doneArr],
+        idNow: data.idNow,
+      };
+    } else if (startState === 'done') {
+      newData = {
+        todoArr: [...data.todoArr],
+        inProgressArr: [...data.inProgressArr],
+        doneArr: [...data.doneArr].filter(el => el.id !== startId),
+        idNow: data.idNow,
+      };
+    }
+    newData[finalState + 'Arr'].splice(index, 0, moveList);
+    setData(newData);
+    localStorage.setItem('IssueData', JSON.stringify(newData));
+  };
+
   return (
     <MainContainer>
       <Header>Issue Tracking Service</Header>
@@ -75,11 +157,15 @@ function App() {
             <ContainerTitle>Todo</ContainerTitle>
             <ContainerListNumber>{data.todoArr.length}</ContainerListNumber>
           </ContainerHeader>
-          <ListContainer>
+          <ListContainer id="todo">
             {data.todoArr.map(el => {
               return (
                 <List
                   draggable
+                  // onDragStart={e => dragStart(e, el)}
+                  // onDragEnter={e => dragEnter(e, el)}
+                  onDragEnd={e => dragEnd(e, el)}
+                  id={el.id}
                   key={el.id}
                   onClick={e => {
                     modalHandler(el, e);
@@ -98,11 +184,15 @@ function App() {
             <ContainerTitle>In Progress</ContainerTitle>
             <ContainerListNumber>{data.inProgressArr.length}</ContainerListNumber>
           </ContainerHeader>
-          <ListContainer>
+          <ListContainer id="inProgress">
             {data.inProgressArr.map(el => {
               return (
                 <List
                   draggable
+                  // onDragStart={e => dragStart(e, el)}
+                  // onDragEnter={e => dragEnter(e, el)}
+                  onDragEnd={e => dragEnd(e, el)}
+                  id={el.id}
                   key={el.id}
                   onClick={e => {
                     modalHandler(el, e);
@@ -121,11 +211,15 @@ function App() {
             <ContainerTitle>Done</ContainerTitle>
             <ContainerListNumber>{data.doneArr.length}</ContainerListNumber>
           </ContainerHeader>
-          <ListContainer>
+          <ListContainer id="done">
             {data.doneArr.map(el => {
               return (
                 <List
                   draggable
+                  // onDragStart={e => dragStart(e, el)}
+                  // onDragEnter={e => dragEnter(e, el)}
+                  onDragEnd={e => dragEnd(e, el)}
+                  id={el.id}
                   key={el.id}
                   onClick={e => {
                     modalHandler(el, e);
